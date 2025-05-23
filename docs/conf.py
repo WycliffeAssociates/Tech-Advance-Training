ed5afbaebd39c5f635853a0df4bf60c780200fbb
John Wood <John_Wood@WycliffeAssociates.org>
2025-05-23 11:02:07 -0400

    Config changes, rebuild

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
 
 # The encoding of source files.
 #
@@ -69,7 +69,7 @@ release = u'0.1'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
